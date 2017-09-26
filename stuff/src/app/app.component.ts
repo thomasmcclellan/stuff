c90afd839ae29e47f5dce3fa9fdea0afe29ee5db
Thomas McClellan <thomasmcclellan87@gmail.com>
2017-09-26 14:58:23 -0400

    Navigation and Routing added

--- stuff/src/app/app.component.ts
+++ stuff/src/app/app.component.ts
@@ -5,10 +5,21 @@ import { ProductService } from './products/product.service';
 selector: 'pm-root',
 template: `
     <div>
-        <h1>{{ pageTitle }}</h1>
-        <pm-products></pm-products>
-    </div>`,
-    providers: [ ProductService ]
+        <nav class='navbar navbar-default'>
+          <div class='container-fluid'>
+            <a class='navbar-brand'>{{ pageTitle }}</a>
+            <ul class='nav navbar-nav'>
+              <li><a [routerLink]="['/welcome']">Home</a></li>
+              <li><a [routerLink]="['/products']">Products</a></li>
+            </ul>
+          </div>
+        </nav>
+        <div class='container'>
+          <router-outlet></router-outlet>
+        </div>
+    </div>
+  `,
+  providers: [ ProductService ]
 })
 
 export class AppComponent {
